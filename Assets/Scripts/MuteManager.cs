a8c8d0e59edeee45ebd23930723ddd4f3817bf4d
Davide <davideluisi002@gmail.com>
2025-04-10 00:04:45 +0200

    Migliorata la grafica del menu
    Aggiornato la grafica dei pulsanti del menu principale e del menu delle opzioni, inserendo delle nuvole al posto dei pulsanti verdi.

--- Assets/Scripts/MuteManager.cs
+++ Assets/Scripts/MuteManager.cs
@@ -3,24 +3,23 @@ using UnityEngine;
 public class MuteManager : MonoBehaviour 
 {
     private bool isMuted;
-    public GameObject soundsOn;
-    public GameObject soundsOff;
+    public GameObject mute;
+    public GameObject unmute;
 
     void Start()
     {
         isMuted = PlayerPrefs.GetInt("MUTED") == 1;
         AudioListener.pause = isMuted;
-        soundsOn.SetActive(!isMuted);
-        soundsOff.SetActive(isMuted);
+        mute.SetActive(!isMuted);
+        unmute.SetActive(isMuted);
     }
 
     public void MutePressed()
     {
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
-        soundsOn.SetActive(!isMuted);
-        soundsOff.SetActive(isMuted);
+        mute.SetActive(!isMuted);
+        unmute.SetActive(isMuted);
         PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
-        
     }
 }
