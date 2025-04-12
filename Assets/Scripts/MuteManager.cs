b72eea941e35fe466aa320e72aeed6222fd1ea57
Davide <davideluisi002@gmail.com>
2025-04-12 02:22:49 +0200

    Miglioramenti grafici
    Cambiato lo sprite del personaggio del giocatore. cambiato la dicitura nel tasto per la Mute Mode, resa ora più chiara, risolto un problema relativo al salvataggio della prefenza sulla Mute Mode.

--- Assets/Scripts/MuteManager.cs
+++ Assets/Scripts/MuteManager.cs
@@ -3,23 +3,23 @@ using UnityEngine;
 public class MuteManager : MonoBehaviour 
 {
     private bool isMuted;
-    public GameObject mute;
-    public GameObject unmute;
+    public GameObject on;
+    public GameObject off;
 
     void Start()
     {
         isMuted = PlayerPrefs.GetInt("MUTED") == 1;
         AudioListener.pause = isMuted;
-        mute.SetActive(!isMuted);
-        unmute.SetActive(isMuted);
+        off.SetActive(!isMuted);
+        on.SetActive(isMuted);
     }
 
     public void MutePressed()
     {
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
-        mute.SetActive(!isMuted);
-        unmute.SetActive(isMuted);
+        off.SetActive(!isMuted);
+        on.SetActive(isMuted);
         PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
     }
 }
